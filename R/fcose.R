7945a6569133b37425b54b2aca2d7d572f0e32e7
Jasen Jackson <jasenjackson@dhcp-10-101-198-175.wireless.northwestern.private>
2019-12-12 15:28:25 -0600

    Added fcose layout

---
+++ R/fcose.R
@@ -0,0 +1,134 @@
+
+fcoseDependency <- function() {
+  list(
+    htmltools::htmlDependency(
+      name = 'fcose', version = '1.0.0',
+      src = system.file('htmlwidgets/lib/fcose', package = 'cytoscape'),
+      script = c('cytoscape-fcose.js')
+    )
+  )
+}
+
+numericDependency <- function() {
+  list(
+    htmltools::htmlDependency(
+      name = 'numeric', version = '1.2.6',
+      src = system.file('htmlwidgets/lib/numeric-1.2.6', package = 'cytoscape'),
+      script = c('numeric-1.2.6.js')
+    )
+  )
+}
+
+coseBaseDependency <- function() {
+  list(
+    htmltools::htmlDependency(
+      name = 'cose-base', version = '1.0.0',
+      src = system.file('htmlwidgets/lib/cose-base', package = 'cytoscape'),
+      script = c('cose-base.js')
+    )
+  )
+}
+
+layoutBaseDependency <- function() {
+  list(
+    htmltools::htmlDependency(
+      name = 'layout-base', version = '1.0.0',
+      src = system.file('htmlwidgets/lib/layout-base', package = 'cytoscape'),
+      script = c('layout-base.js')
+    )
+  )
+}
+
+
+fcose_layout <- function(cytoscape,
+                         name = 'fcose',
+                         quality = 'default',
+                         randomize = TRUE,
+                         animate = TRUE,
+                         animationDuration = 1000,
+                         animationEasing = NULL,
+                         fit = TRUE,
+                         padding = 30,
+                         nodeDimensionsIncludeLabels = FALSE,
+                         uniformNodeDimensions = FALSE,
+                         packComponents = TRUE,
+
+                         # spectral layout options
+                         samplingType = TRUE,
+                         sampleSize = 25,
+                         nodeSeparation = 75,
+                         piTol = 0.0000001,
+
+                        # incremental layout options
+                        nodeRepulsion = 4500,
+                        idealEdgeLength = 50,
+                        edgeElasticity = 0.45,
+                        nestingFactor = 0.1,
+                        numIter = 2500,
+                        tile = TRUE,
+                        tilingPaddingVertical = 10,
+                        tilingPaddingHorizontal = 10,
+                        gravity = 0.25,
+                        gravityRangeCompound = 1.5,
+                        gravityCompound = 1.0,
+                        gravityRange = 3.8,
+                        initialEnergyOnIncremental = 0.3,
+
+                        # layout event callbacks
+                        ready = htmlwidgets::JS('() => {}'),
+                        stop = htmlwidgets::JS('() => {}'),
+                        ...){
+
+   layout <- list(name = 'fcose',
+       name = 'fcose',
+       quality = quality,
+       randomize = randomize,
+       animate = animate,
+       animationDuration = animationDuration,
+       animationEasing = animationEasing,
+       fit = fit,
+       padding = padding,
+       nodeDimensionsIncludeLabels = nodeDimensionsIncludeLabels,
+       uniformNodeDimensions = uniformNodeDimensions,
+       packComponents = packComponents,
+
+       # spectral layout options
+       samplingType = samplingType,
+       sampleSize = sampleSize,
+       nodeSeparation = nodeSeparation,
+       piTol = piTol,
+
+      # incremental layout options
+      nodeRepulsion = nodeRepulsion,
+      idealEdgeLength = idealEdgeLength,
+      edgeElasticity = edgeElasticity,
+      nestingFactor = nestingFactor,
+      numIter = numIter,
+      tile = tile,
+      tilingPaddingVertical = tilingPaddingVertical,
+      tilingPaddingHorizontal = tilingPaddingHorizontal,
+      gravity = gravity,
+      gravityRangeCompound = gravityRangeCompound,
+      gravityCompound = gravityCompound,
+      gravityRange = gravityRange,
+      initialEnergyOnIncremental = initalEnergyOnIncremental,
+
+      # layout event callbacks
+      ready = ready,
+      stop = stop
+    )
+
+    layout <- modifyList(layout, list(...))
+    layout <- Filter(Negate(function(x) is.null(unlist(x))), layout)
+
+    cytoscape$x$layout <- layout
+
+    # add fcose dependencies here
+    if (is.null(cytoscape$dependencies)) {
+      cytoscape$dependencies <- c(numericDependency(), coseBaseDependency(), layoutBaseDependency())
+    } else {
+      cytoscape$dependencies <- c(numericDependency(), coseBaseDependency(), layoutBaseDependency())
+    }
+
+    return(cytoscape)
+}
